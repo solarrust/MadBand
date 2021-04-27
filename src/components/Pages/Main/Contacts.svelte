4c601d297a60c9bead6f6beede57f3359db5a813
Alena Batitskaya <solar.rust@gmail.com>
2021-04-27 20:40:06 +0300

    Footer animation

--- src/components/Pages/Main/Contacts.svelte
+++ src/components/Pages/Main/Contacts.svelte
@@ -274,8 +274,60 @@
       var(--green) 15px
     );
     background-size: 15px;
-    animation: line-grow 1s ease-in-out infinite alternate,
-      allCases 10s infinite;
+    animation: allCases 6s infinite ease-in-out alternate-reverse;
+  }
+
+  :global(.lines-pattern__row:nth-child(2)) {
+    background-image: linear-gradient(
+      to right,
+      transparent 6px,
+      var(--green) 6px,
+      var(--green) 8px,
+      transparent 8px
+    );
+    animation-delay: 1s;
+  }
+
+  :global(.lines-pattern__row:nth-child(3)) {
+    background-image: linear-gradient(
+      to right,
+      transparent 12px,
+      var(--green) 12px
+    );
+    animation-delay: 2s;
+  }
+
+  :global(.lines-pattern__row:nth-child(4)) {
+    background-image: linear-gradient(
+      to right,
+      transparent 5px,
+      var(--green) 5px,
+      var(--green) 9px,
+      transparent 9px
+    );
+    animation-delay: 1s;
+  }
+
+  :global(.lines-pattern__row:nth-child(5)) {
+    background-image: linear-gradient(
+      to right,
+      transparent 10px,
+      var(--green) 10px,
+      var(--green) 15px,
+      transparent 15px
+    );
+    animation-delay: 4s;
+  }
+
+  :global(.lines-pattern__row:nth-child(6)) {
+    background-image: linear-gradient(
+      to right,
+      transparent 3px,
+      var(--green) 3px,
+      var(--green) 11px,
+      transparent 11px
+    );
+    animation-delay: 3s;
   }
 
   @keyframes allCases {
@@ -283,45 +335,7 @@
       background-position-x: 0;
     }
     to {
-      background-position-x: 150px;
-    }
-  }
-
-  @keyframes line-grow {
-    from {
-      background-image: linear-gradient(
-        to right,
-        transparent 14px,
-        var(--green) 15px
-      );
-    }
-    25% {
-      background-image: linear-gradient(
-        to right,
-        transparent 12px,
-        var(--green) 12px
-      );
-    }
-    50% {
-      background-image: linear-gradient(
-        to right,
-        transparent 10px,
-        var(--green) 10px
-      );
-    }
-    78% {
-      background-image: linear-gradient(
-        to right,
-        transparent 8px,
-        var(--green) 8px
-      );
-    }
-    to {
-      background-image: linear-gradient(
-        to right,
-        transparent 7px,
-        var(--green) 7px
-      );
+      background-position-x: 750px;
     }
   }
 </style>
