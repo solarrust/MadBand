b16a662e32e18980ed1f2de70f976f0203a33542
Alena Batitskaya <solar.rust@gmail.com>
2021-05-16 15:07:11 +0300

    Lines pattern gsap animation

--- src/components/Patterns/Lines.svelte
+++ src/components/Patterns/Lines.svelte
@@ -1,7 +1,48 @@
 <script>
   import { onMount } from "svelte";
+  import { gsap } from "gsap";
 
-  onMount(() => {});
+  onMount(() => {
+    let parentWidth = document.querySelector(".lines-pattern").clientWidth;
+    let rows = Array.from(document.querySelectorAll(".lines-pattern__row"));
+
+    let direction = "right";
+    let oldx = 0;
+    let anim;
+    let mouseMoveMethod = function (e) {
+      if (e.pageX < oldx) {
+        direction = "left";
+      } else if (e.pageX > oldx) {
+        direction = "right";
+      }
+
+      oldx = e.pageX;
+
+      return direction;
+    };
+
+    rows.forEach((row) => {
+      function linesAnim(direction) {
+        return gsap.to(row, {
+          duration: 30,
+          // repeat: -1,
+          ease: "none",
+          backgroundPositionX:
+            direction === "right" ? parentWidth : -parentWidth,
+          paused: true,
+        });
+      }
+
+      row.addEventListener("mousemove", (e) => {
+        direction = mouseMoveMethod(e);
+        linesAnim(direction).play();
+      });
+
+      row.addEventListener("mouseout", (e) => {
+        linesAnim(direction).pause();
+      });
+    });
+  });
 </script>
 
 <div class="lines-pattern">
@@ -18,7 +59,7 @@
     height: 50vh;
   }
 
-  :global(.lines-pattern__row) {
+  .lines-pattern__row {
     height: calc(100% / 6);
     background-image: linear-gradient(
       to right,
@@ -26,10 +67,11 @@
       var(--green) 15px
     );
     background-size: 15px;
-    animation: allCases 6s infinite ease-in-out alternate-reverse;
+    background-position: 0;
+    /*animation: allCases 6s infinite ease-in-out alternate-reverse;*/
   }
 
-  :global(.lines-pattern__row:nth-child(2)) {
+  .lines-pattern__row:nth-child(2) {
     background-image: linear-gradient(
       to right,
       transparent 6px,
@@ -37,19 +79,19 @@
       var(--green) 8px,
       transparent 8px
     );
-    animation-delay: 1s;
+    /*animation-delay: 1s;*/
   }
 
-  :global(.lines-pattern__row:nth-child(3)) {
+  .lines-pattern__row:nth-child(3) {
     background-image: linear-gradient(
       to right,
       transparent 12px,
       var(--green) 12px
     );
-    animation-delay: 2s;
+    /*animation-delay: 2s;*/
   }
 
-  :global(.lines-pattern__row:nth-child(4)) {
+  .lines-pattern__row:nth-child(4) {
     background-image: linear-gradient(
       to right,
       transparent 5px,
@@ -57,10 +99,10 @@
       var(--green) 9px,
       transparent 9px
     );
-    animation-delay: 1s;
+    /*animation-delay: 1s;*/
   }
 
-  :global(.lines-pattern__row:nth-child(5)) {
+  .lines-pattern__row:nth-child(5) {
     background-image: linear-gradient(
       to right,
       transparent 10px,
@@ -68,10 +110,10 @@
       var(--green) 15px,
       transparent 15px
     );
-    animation-delay: 4s;
+    /*animation-delay: 4s;*/
   }
 
-  :global(.lines-pattern__row:nth-child(6)) {
+  .lines-pattern__row:nth-child(6) {
     background-image: linear-gradient(
       to right,
       transparent 3px,
@@ -79,15 +121,15 @@
       var(--green) 11px,
       transparent 11px
     );
-    animation-delay: 3s;
+    /*animation-delay: 3s;*/
   }
 
-  @keyframes allCases {
-    from {
-      background-position-x: 0;
-    }
-    to {
-      background-position-x: 750px;
-    }
-  }
+  /*@keyframes allCases {*/
+  /*  from {*/
+  /*    background-position-x: 0;*/
+  /*  }*/
+  /*  to {*/
+  /*    background-position-x: 750px;*/
+  /*  }*/
+  /*}*/
 </style>
