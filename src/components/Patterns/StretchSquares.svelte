6370ee44b25c1d9deaca2a39dc17f3a075252019
Alena Batitskaya <solar.rust@gmail.com>
2021-05-29 00:14:05 +0300

    Studio page: blocks pattern animation

--- src/components/Patterns/StretchSquares.svelte
+++ src/components/Patterns/StretchSquares.svelte
@@ -1,5 +1,48 @@
 <script>
+  import { gsap, TimelineLite } from "gsap";
+  import { ScrollTrigger } from "gsap/ScrollTrigger";
+  import { onMount } from "svelte";
+  gsap.registerPlugin(ScrollTrigger);
+
   export let extraClass;
+
+  onMount(() => {
+    let animLines = Array.from(
+      document.querySelectorAll(".square-pattern__line")
+    );
+
+    let anim = gsap.timeline();
+
+    animLines.forEach((line, i) => {
+      line.style.height = `${10 * (i + 1)}%`;
+
+      anim.to(line, {
+        height: `${10 * (animLines.length - i)}%`,
+      });
+
+      ScrollTrigger.create({
+        animation: anim,
+        trigger: ".square-pattern",
+        start: "top 50%",
+        end: "bottom 50%",
+        toggleActions: "play none none reverse",
+        scrub: 1,
+        // markers: true,
+      });
+
+      window.onscroll = () => {
+        let oldPos = anim.scrollTrigger.start;
+        anim.scrollTrigger.refresh();
+
+        if (oldPos !== anim.scrollTrigger.start) {
+          anim.scrollTrigger.refresh();
+          console.log("anim refreshed");
+        }
+      };
+
+      return () => anim.kill();
+    });
+  });
 </script>
 
 <div class="square-pattern {extraClass}">
@@ -42,55 +85,76 @@
 
   .square-pattern__line {
     display: flex;
+    flex-grow: 0;
     width: 100%;
     height: 25%;
+    /*animation-play-state: paused;*/
+    /*animation-timing-function: ease-in-out;*/
+    /*animation-duration: 2s;*/
   }
 
-  .square-pattern__line:first-child {
-    animation: stretch ease-in-out 5s infinite alternate;
-  }
+  /*.square-pattern__line:nth-child(2),*/
+  /*.square-pattern__line:nth-child(3) {*/
+  /*  flex-grow: 1;*/
+  /*  flex-shrink: 2;*/
+  /*}*/
 
-  .square-pattern__line:nth-child(2) {
-    animation: stretch-small ease-in-out 5s infinite alternate-reverse;
-  }
+  /*.square-pattern__line:first-child {*/
+  /*  animation-name: stretch;*/
+  /*  animation-direction: alternate;*/
+  /*}*/
 
-  .square-pattern__line:nth-child(3) {
-    animation: stretch-small ease-in-out 5s infinite alternate;
-  }
+  /*.square-pattern__line:nth-child(2) {*/
+  /*  animation-name: stretch-small;*/
+  /*  animation-direction: alternate-reverse;*/
+  /*}*/
 
-  .square-pattern__line:last-child {
-    animation: stretch ease-in-out 5s infinite alternate-reverse;
-  }
+  /*.square-pattern__line:nth-child(3) {*/
+  /*  animation-name: stretch-small;*/
+  /*  animation-direction: alternate;*/
+  /*}*/
+
+  /*.square-pattern__line:last-child {*/
+  /*  animation-name: stretch;*/
+  /*  animation-direction: alternate-reverse;*/
+  /*}*/
+
+  /*:global(.square-pattern__line.active) {*/
+  /*  animation-play-state: running;*/
+  /*}*/
 
   .square-pattern__child {
     width: 20%;
     height: 100%;
     /*border: 1px solid red;*/
-    background-image: linear-gradient(
-      75deg,
-      var(--bkg-color) 0,
-      var(--bkg-color) 22%,
-      var(--thistle) 22%,
-      var(--thistle) 80%,
-      var(--bkg-color) 80%
-    );
+    /*background-image: linear-gradient(*/
+    /*  75deg,*/
+    /*  var(--bkg-color) 0,*/
+    /*  var(--bkg-color) 22%,*/
+    /*  var(--thistle) 22%,*/
+    /*  var(--thistle) 80%,*/
+    /*  var(--bkg-color) 80%*/
+    /*);*/
+    clip-path: polygon(0 0, 52% 0, 100% 60%, 100% 100%, 50% 100%, 0 30%);
+
+    background-color: var(--thistle);
   }
 
-  @keyframes stretch {
-    0% {
-      height: 50%;
-    }
-    100% {
-      height: 15%;
-    }
-  }
+  /*@keyframes stretch {*/
+  /*  0% {*/
+  /*    height: 50%;*/
+  /*  }*/
+  /*  100% {*/
+  /*    height: 15%;*/
+  /*  }*/
+  /*}*/
 
-  @keyframes stretch-small {
-    0% {
-      height: 20%;
-    }
-    100% {
-      height: 35%;
-    }
-  }
+  /*@keyframes stretch-small {*/
+  /*  0% {*/
+  /*    height: 20%;*/
+  /*  }*/
+  /*  100% {*/
+  /*    height: 35%;*/
+  /*  }*/
+  /*}*/
 </style>
